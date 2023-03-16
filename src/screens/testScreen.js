77e35350cf46b32daf6ea1dd28a8c03f2bb26423
Hashen Kottegoda <thkottegoda@gmail.com>
2023-03-17 02:08:28 +0530

    tets

---
+++ src/screens/testScreen.js
@@ -0,0 +1,6 @@
+//gen code here
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../store/Job";
