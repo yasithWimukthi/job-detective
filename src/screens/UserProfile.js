1559cef5e1a9846314a102be907662f59b770cf7
yasith <wimukthiyasith@gmail.com>
2023-03-18 21:51:38 +0530

    implement user profile

--- src/screens/UserProfile.js
+++ src/screens/UserProfile.js
@@ -2,30 +2,6 @@ import React, { Component } from "react";
 import {StyleSheet, View, Image, Text, TouchableOpacity} from "react-native";
 import { Button, Actionsheet, useDisclose, Box, Center, NativeBaseProvider } from "native-base";
 
-
-
-function BottomSelector() {
-
-    return <Center>
-        <Actionsheet isOpen={isOpen} onClose={onClose}>
-            <Actionsheet.Content>
-                <Box w="100%" h={60} px={4} justifyContent="center">
-                    <Text fontSize="16" color="gray.500" _dark={{
-                        color: "gray.300"
-                    }}>
-                        Albums
-                    </Text>
-                </Box>
-                <Actionsheet.Item>Delete</Actionsheet.Item>
-                <Actionsheet.Item isDisabled>Share</Actionsheet.Item>
-                <Actionsheet.Item>Play</Actionsheet.Item>
-                <Actionsheet.Item>Favourite</Actionsheet.Item>
-                <Actionsheet.Item>Cancel</Actionsheet.Item>
-            </Actionsheet.Content>
-        </Actionsheet>
-    </Center>;
-}
-
 function UserProfile(props) {
 
     const {
