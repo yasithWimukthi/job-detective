4a6e430e5c935c20bacfb484448ed6575192e5c1
Hashen Kottegoda <thkottegoda@gmail.com>
2023-03-18 21:13:08 +0530

    resolved conflicts

--- App.js
+++ App.js
@@ -16,14 +16,10 @@ import { NativeBaseProvider, Text, Box } from "native-base";
 import JobPost from "./src/screens/JobPosts";
 import JobCreate from "./src/screens/JobCreate";
 import JobMyPosts from "./src/screens/JobMyPosts";
-<<<<<<< HEAD
 import AddNewCompany from "./src/screens/AddNewCompany";
-=======
 import LoginScreen from "./src/screens/LoginScreen";
 import JobView from "./src/screens/JobView";
 import JobUpdate from "./src/screens/JobUpdate";
-
->>>>>>> 52c93ed8df1515e64fa761d5386c2374ebef2c5d
 
 function CustomDrawerContent(props) {
   return (
@@ -46,9 +42,8 @@ function MyDrawer() {
         <Drawer.Screen name="Companies" component={CompaniesHome} />
         <Drawer.Screen name="Homes" component={HomeScreen} />
         <Drawer.Screen name="Jobs" component={JobPost} />
-          <Drawer.Screen name="Register" component={RegisterScreen} />
-          <Drawer.Screen name="Login" component={LoginScreen} />
-
+        <Drawer.Screen name="Register" component={RegisterScreen} />
+        <Drawer.Screen name="Login" component={LoginScreen} />
       </Drawer.Navigator>
     </NativeBaseProvider>
   );
